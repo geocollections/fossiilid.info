6acb26e189e918c001b1ab527a6391b398f6f47c
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2023-11-01 10:46:03 +0200

    feat: load scripts async

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,11 +22,15 @@ export default defineNuxtConfig({
         { name: "apple-mobile-web-app-status-bar-style", content: "default" },
       ],
       script: [
-        { src: "https://unpkg.com/jquery@3.3.1/dist/jquery.min.js" },
+        {
+          src: "https://unpkg.com/jquery@3.3.1/dist/jquery.min.js",
+          async: true,
+        },
         {
           src: "https://unpkg.com/@fancyapps/fancybox@3.4.2/dist/jquery.fancybox.min.js",
+          async: true,
         },
-        { src: "/js/matomo.js" },
+        { src: "/js/matomo.js", async: true },
       ],
       link: [
         {
