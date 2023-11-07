7b59ed00e761ec69294b5c20474a740a883ede7a
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2023-11-07 14:03:04 +0200

    refactor: remove fas icon

--- pages/search.vue
+++ pages/search.vue
@@ -603,9 +603,12 @@ async function buildPopupContent(layer: Circle | Rectangle | Polygon) {
 
   if (numberOfDrawnLayers > 1) {
     const button = document.createElement("button");
+    button.className =
+      "border flex items-center px-2 py-1 rounded bg-tomato text-white";
     button.innerHTML = `
-        <span class="fa fa-search"></span>
-        ${t("advancedsearch.js_map_popup_linkText")}
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+        </svg>${t("advancedsearch.js_map_popup_linkText")}
 `;
     button.onclick = () => {
       map.value?.closePopup();
