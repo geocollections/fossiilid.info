9bcc9d589d3ef569e73af38dd0dde8e0147337ac
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2023-11-17 12:08:20 +0200

    refactor: remove unused state

--- stores/root.ts
+++ stores/root.ts
@@ -3,15 +3,9 @@ import { defineStore } from "pinia";
 export const useRootStore = defineStore("root", {
   state: () => {
     return {
-      errorMessage: null as string | null,
-      activeTab: "overview",
-      lang: "en",
       mode: "in_baltoscandia",
       searchParameters: {
-        selectedImages: { page: 1, paginateBy: 30, allowPaging: true },
-        images: { page: 1, paginateBy: 20, allowPaging: true },
         species: { page: 1, paginateBy: 20 },
-        advancedSearch: { page: 1, paginateBy: 100, madDataPaginateBy: 1000 },
         specimens: {
           page: 1,
           paginateBy: 10,
