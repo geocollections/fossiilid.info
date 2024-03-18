94c21c2dc18dba0a5d90267a5b92c8a7201dbd69
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2024-03-18 14:26:46 +0200

    fix: correct the reference item style

--- pages/[id].vue
+++ pages/[id].vue
@@ -542,22 +542,28 @@
                 {{ reference.year }}.
               </a>
               <span>{{ reference.title }}.</span>
-
               <span v-if="reference.journal_name">
+                {{ " " }}
                 <em>{{ reference.journal_name }}</em>
                 {{ " " }}
+                <span v-if="reference.volume">
                 <strong>{{ reference.volume }}</strong>
-                ,
-                <span v-if="reference.number">{{ reference.number }},</span>
-                <span v-if="isDefinedAndNotNull(reference.pages)">
-                  {{ reference.pages }}.
+                <span>,</span>
                 </span>
+                {{ " " }}
+                <span v-if="reference.number"> {{ reference.number }},</span>
+                {{ " " }}
+                <span v-if="isDefinedAndNotNull(reference.pages)"> {{ reference.pages }}.</span>
               </span>
-              <span v-if="isDefinedAndNotNull(reference.book)">
+              <span v-else-if="isDefinedAndNotNull(reference.book)">
                 <em>{{ reference.book }}</em>
                 , pp. {{ reference.pages }}.
               </span>
-
+              <span v-else> pp. {{ reference.pages }}.</span>
+              <span v-if="reference.publisher">
+                {{ " " }}
+                {{ reference.publisher }}.
+              </span>
               <span v-if="reference.doi">
                 <NuxtLink
                   :href="`https://doi.org/${reference.doi}`"
