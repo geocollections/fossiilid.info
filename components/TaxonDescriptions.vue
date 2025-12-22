903b21d51cf9f9604552db5d57e27c40c2a45c1f
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2025-12-22 11:43:47 +0200

    feature: sort descriptions and make them foldable

--- components/TaxonDescriptions.vue
+++ components/TaxonDescriptions.vue
@@ -11,13 +11,34 @@ const { data: taxonDescriptions } = await useNewApiFetch<{
 }>(`/taxa/${props.taxon.id}/taxon-descriptions/`, {
   query: {
     taxon: props.taxon.id,
-    fields: "reference.id,reference.reference,description",
+    fields: "reference.id,reference.reference,reference.year,description",
     expand: "reference",
     format: "json",
     ordering: "-id",
   },
 });
-const descriptions = computed(() => taxonDescriptions.value?.results);
+const descriptions = computed(() => taxonDescriptions.value?.results ?? []);
+
+const sortedDescriptions = computed(() => {
+  return descriptions.value.toSorted((a, b) => {
+    const parsedAYear = Number.parseInt(a.reference.year, 10);
+    const parsedBYear = Number.parseInt(b.reference.year, 10);
+
+    if (Number.isNaN(parsedAYear) && Number.isNaN(parsedBYear))
+      return 0;
+    else if (Number.isNaN(parsedAYear))
+      return 1;
+    else if (Number.isNaN(parsedBYear))
+      return -1;
+
+    if (parsedAYear > parsedBYear)
+      return -1;
+    else if (parsedAYear < parsedBYear)
+      return 1;
+
+    return 0;
+  });
+});
 </script>
 
 <template>
@@ -28,16 +49,18 @@ const descriptions = computed(() => taxonDescriptions.value?.results);
     <template #header>
       {{ $t("header.f_taxon_description_diagnosis") }}
     </template>
-    <div v-for="(item, index) in descriptions" :key="index">
-      <h3 v-if="item.description">
-        <a
-          v-if="item.reference"
-          :href="`https://kirjandus.geoloogia.info/reference/${item.reference.id}`"
-        >
-          <strong>{{ item.reference.reference }}</strong>
-        </a>
-      </h3>
-      <div v-html="item.description" />
-    </div>
+    <Foldable :el-length="sortedDescriptions.length">
+      <div v-for="(item, index) in sortedDescriptions" :key="index">
+        <h3 v-if="item.description">
+          <a
+            v-if="item.reference"
+            :href="`https://kirjandus.geoloogia.info/reference/${item.reference.id}`"
+          >
+            <strong>{{ item.reference.reference }}</strong>
+          </a>
+        </h3>
+        <div v-html="item.description" />
+      </div>
+    </Foldable>
   </UCard>
 </template>
