e2bfcdd5cfe6429fe66b37f1fe75804f0e31fa05
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2025-12-22 12:27:54 +0200

    feat: add reference to type specimen

--- components/TaxonReferences.vue
+++ components/TaxonReferences.vue
@@ -22,46 +22,15 @@ const references = computed(() => referencesRes.value?.results ?? []);
       {{ $t("header.f_taxon_references") }}
     </template>
     <Foldable :el-length="references.length">
-      <div
+      <template
         v-for="(reference, idx) in references"
         :key="idx"
-        :class="idx === references.length - 1 ? '' : 'my-3'"
-        style="padding-left: 3em; text-indent: -3em"
       >
-        <a :href="`https://kirjandus.geoloogia.info/reference/${reference.id}`">
-          {{ reference.author }}
-          {{ reference.year }}.
-        </a>
-        <span>{{ reference.title }}.</span>
-        <span v-if="reference.journal_name">
-          {{ " " }}
-          <em>{{ reference.journal_name }}</em>
-          {{ " " }}
-          <span v-if="reference.volume">
-            <strong>{{ reference.volume }}</strong>
-            <span>,</span>
-          </span>
-          {{ " " }}
-          <span v-if="reference.number"> {{ reference.number }},</span>
-          {{ " " }}
-          <span v-if="isDefinedAndNotNull(reference.pages)">
-            {{ reference.pages }}.</span>
-        </span>
-        <span v-else-if="isDefinedAndNotNull(reference.book)">
-          {{ " " }}
-          <em>{{ reference.book }}</em>, pp. {{ reference.pages }}.
-        </span>
-        <span v-else> pp. {{ reference.pages }}.</span>
-        <span v-if="reference.publisher">
-          {{ " " }}
-          {{ reference.publisher }}.
-        </span>
-        <span v-if="reference.doi">
-          <NuxtLink :href="`https://doi.org/${reference.doi}`">
-            DOI:{{ reference.doi }}
-          </NuxtLink>
-        </span>
-      </div>
+        <ReferenceItem
+          :class="idx === references.length - 1 ? '' : 'my-3'"
+          :reference="reference"
+        />
+      </template>
     </Foldable>
   </UCard>
 </template>
