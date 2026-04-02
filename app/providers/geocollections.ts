54a3f6c3f87c4f90c29f6744f44447992e6fe0a6
Marko Peedosk <mpeedo@taltech.ee>
2026-04-02 09:41:23 +0300

    fix(image): migrate to @nuxt/image v2 provider syntax

--- app/providers/geocollections.ts
+++ app/providers/geocollections.ts
@@ -1,20 +1,20 @@
 import { joinURL } from "ufo";
+import { defineProvider } from "@nuxt/image/runtime";
 
-// import {} from '~image'
-import type { ProviderGetImage } from "@nuxt/image";
-
-export const getImage: ProviderGetImage = (
-  src: string,
-  { modifiers, baseURL } = {},
-  _ctx,
-) => {
-  if (modifiers) {
-    const { size } = modifiers;
-    return {
-      url: joinURL(baseURL as string, size as string, src),
-    };
-  }
-  return {
-    url: joinURL(baseURL as string, src),
-  };
-};
+export default defineProvider({ 
+	getImage (
+	  src: string,
+	  { modifiers, baseURL } = {},
+	  _ctx,
+	) {
+	  if (modifiers) {
+		const { size } = modifiers;
+		return {
+		  url: joinURL(baseURL as string, size as string, src),
+		};
+	  }
+	  return {
+		url: joinURL(baseURL as string, src),
+	  };
+	}
+});
